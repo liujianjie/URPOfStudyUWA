8c15075197b68cb6d2b23ec9e58de6e2121d33d6
liujianjie <liujianjie@galasports.com>
2024-01-17 13:14:22 +0800

    ''

--- URPOfStudyUWA/Assets/CustomRP/Runtime/CameraRenderer.Editor.cs
+++ URPOfStudyUWA/Assets/CustomRP/Runtime/CameraRenderer.Editor.cs
@@ -1,117 +1,96 @@
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting;
+using UnityEditor;
 using UnityEngine;
+using UnityEngine.Profiling;
 using UnityEngine.Rendering;
 
 public partial class CameraRenderer
 {
-
-    ScriptableRenderContext context;
-    Camera camera;
-    public void Render(ScriptableRenderContext context, Camera camera)
+    // 声明这个方法
+    partial void DrawUnsupportedShaders();
+    partial void DrawGizmos();
+    partial void PrepareForSceneWindow();
+    partial void PrepareBuffer();
+#if UNITY_EDITOR
+    // SRP不支持的着色器标签类型
+    static ShaderTagId[] legacyShaderTagIds =
     {
-        this.context = context;
-        this.camera = camera;
-
-        if (!Cull())
-        {
-            return;
-        }
-
-        Setup();
-        // 绘制几何体
-        DrawVisibleGeometry();
-        // 绘制SRP不支持的着色器类型
-        DrawUnsupportedShaders();
-        Submit();
-    }
-    /// <summary>
-    /// 设置相机的属性和矩阵
-    /// </summary>
-    void Setup()
-    {
-        // 设置相机的属性和矩阵
-        context.SetupCameraProperties(camera);
-
-        // 为保证下一帧渲染正确，需要清除上一帧的渲染结果
-        buffer.ClearRenderTarget(true, true, Color.clear);
-
-        // 自定义渲染
-        buffer.BeginSample(bufferName); // 开始采样
-        ExecuteBuffer();    // 执行缓冲区命令
-
-    }
-    /// <summary>
-    /// 执行缓冲区命令：使用和清除缓冲区通常是配套使用的
-    /// </summary>
-    void ExecuteBuffer()
-    {
-        context.ExecuteCommandBuffer(buffer);
-        buffer.Clear();
-    }
-    static ShaderTagId unlitShaderTagId = new ShaderTagId("SRPDefaultUnlit");
-
-
-    /// <summary>
-    /// 绘制可见物
-    /// </summary>
-    void DrawVisibleGeometry()
-    {
-        // 设置绘制顺序和指定渲染相机
-        var sortingSettings = new SortingSettings(camera)
-        {
-            criteria = SortingCriteria.CommonOpaque
-        };
-        // 设置绘制的shader Pass 和排序模式
-        var drawingSettings = new DrawingSettings(unlitShaderTagId, sortingSettings);
-        // 设置哪些类型的渲染队列可以被绘制
-        var filteringSettings = new FilteringSettings(RenderQueueRange.all);
-
-        // 1.绘制不透明物体
-        context.DrawRenderers(cullingResults, ref drawingSettings, ref filteringSettings);
-
-        // 2.绘制天空盒
-        context.DrawSkybox(camera);
-
-        sortingSettings.criteria = SortingCriteria.CommonTransparent;
-        drawingSettings.sortingSettings = sortingSettings;
-        // 只绘制RenderQueue为Transparent的物体
-        filteringSettings.renderQueueRange = RenderQueueRange.transparent;
-        // 3.绘制透明物体
-        context.DrawRenderers(cullingResults, ref drawingSettings, ref filteringSettings);
-    }
-    /// <summary>
-    /// 提交缓冲区渲染命令
-    /// </summary>
-    void Submit()
-    {
-        buffer.EndSample(bufferName);
-        ExecuteBuffer();
-        context.Submit();       // 提交缓冲区渲染命令才进行这一帧的渲染
-    }
-    // 缓冲区，用来绘制场景的其它几何图像
-    const string bufferName = "My Render Camera";
-    CommandBuffer buffer = new CommandBuffer
-    {
-        name = bufferName
+        new ShaderTagId("Always"),
+        new ShaderTagId("ForwardBase"),
+        new ShaderTagId("PrepassBase"),
+        new ShaderTagId("Vertex"),
+        new ShaderTagId("VertexLMRGBM"),
+        new ShaderTagId("VertexLM"),
     };
+    // 绘制成使用错误材质的粉红颜色
+    static Material errorMaterial;
 
-    // 存储剔除后的结果数据
-    CullingResults cullingResults;
     /// <summary>
-    /// 剔除
+    /// 绘制SRP不支持的着色器类型
     /// </summary>
-    /// <returns></returns>
-    bool Cull()
+    partial void DrawUnsupportedShaders()
     {
-        ScriptableCullingParameters p;
-        // 获取相机的剔除参数
-        if (camera.TryGetCullingParameters(out p))
+        // 不支持的ShaderTag类型我们使用错误材质专用Shader来渲染（粉色颜色）
+        if (errorMaterial == null)
         {
-            // 剔除
-            cullingResults = context.Cull(ref p);
-            return true;
+            errorMaterial = new Material(Shader.Find("Hidden/InternalErrorShader"));
         }
-        return false;
+
+
+        // 数组第一个元素用来构造DrawingSettings对象的时候设置
+        var drawingSettings = new DrawingSettings(legacyShaderTagIds[0], new SortingSettings(camera))
+        {
+            overrideMaterial = errorMaterial
+        };
+        for (int i = 1; i < legacyShaderTagIds.Length; i++)
+        {
+            // 遍历数组逐个设置着色器的PassName，从i=1开始
+            drawingSettings.SetShaderPassName(i, legacyShaderTagIds[i]);
+        }
+        // 使用默认设置即可，反正画出来的都是不支持的
+        var filteringSettings = FilteringSettings.defaultValue;
+        // 绘制不支持的shadertag类型的物体
+        context.DrawRenderers(cullingResults, ref drawingSettings, ref filteringSettings);
     }
+    /// <summary>
+    /// 绘制Gizmos
+    /// </summary>
+    partial void DrawGizmos()
+    {
+        if (Handles.ShouldRenderGizmos())
+        {
+            context.DrawGizmos(camera, GizmoSubset.PreImageEffects);
+            context.DrawGizmos(camera, GizmoSubset.PostImageEffects);
+        }
+    }
+    /// <summary>
+    /// 在Game视图绘制的几何体也绘制到scene使用中
+    /// </summary>
+    partial void PrepareForSceneWindow()
+    {
+        if (camera.cameraType == CameraType.SceneView)
+        {
+            // 如果切换到了scene视图，调用此方法完成绘制
+            ScriptableRenderContext.EmitWorldGeometryForSceneView(camera);
+        }
+    }
+#endif
+#if UNITY_EDITOR
+    string SampleName { get; set; }
+    /// <summary>
+    /// 设置缓冲区的名字
+    /// </summary>
+    partial void PrepareBuffer()
+    {
+        // 设置一下只有在编辑器模式下才分配内存
+        Profiler.BeginSample("Editor Only");
+        buffer.name = SampleName= camera.name;
+        Profiler.EndSample();
+    }
+#else
+    const string SampleName = bufferName;
+#endif
+
 }
--- URPOfStudyUWA/Assets/CustomRP/Runtime/CameraRenderer.cs
+++ URPOfStudyUWA/Assets/CustomRP/Runtime/CameraRenderer.cs
@@ -1,52 +1,133 @@
 using System.Collections;
 using System.Collections.Generic;
+using UnityEditor;
 using UnityEngine;
 using UnityEngine.Rendering;
 
 public partial class CameraRenderer
 {
-    // 声明这个方法
-    partial void DrawUnsupportedShaders();
-#if UNITY_EDITOR
-    // SRP不支持的着色器标签类型
-    static ShaderTagId[] legacyShaderTagIds =
+    ScriptableRenderContext context;
+    Camera camera;
+    public void Render(ScriptableRenderContext context, Camera camera)
     {
-        new ShaderTagId("Always"),
-        new ShaderTagId("ForwardBase"),
-        new ShaderTagId("PrepassBase"), 
-        new ShaderTagId("Vertex"),
-        new ShaderTagId("VertexLMRGBM"),
-        new ShaderTagId("VertexLM"),
-    };
-    // 绘制成使用错误材质的粉红颜色
-    static Material errorMaterial;
+        this.context = context;
+        this.camera = camera;
+        // 设置缓冲区的名字
+        PrepareBuffer();
+
+        // 在game视图绘制的几何体也绘制到scene视图中
+        PrepareForSceneWindow();
+
+        if (!Cull())
+        {
+            return;
+        }
+
+        Setup();
+        // 绘制几何体
+        DrawVisibleGeometry();
+        // 绘制SRP不支持的着色器类型
+        DrawUnsupportedShaders();
+        // 绘制Gizmos
+        DrawGizmos();
+        Submit();
+    }
+    /// <summary>
+    /// 设置相机的属性和矩阵
+    /// </summary>
+    void Setup()
+    {
+        // 设置相机的属性和矩阵
+        context.SetupCameraProperties(camera);
+
+        // 得到相机的clear flags
+        CameraClearFlags flags = camera.clearFlags;
+
+        // 设置相机的清除状态
+        buffer.ClearRenderTarget(flags <= CameraClearFlags.Depth, flags == CameraClearFlags.Color, 
+            flags == CameraClearFlags.Color ? camera.backgroundColor.linear : Color.white);
+        // 为保证下一帧渲染正确，需要清除上一帧的渲染结果
+        //buffer.ClearRenderTarget(true, true, Color.clear);
+
+        // 自定义渲染
+        buffer.BeginSample(SampleName); // 开始采样
+        ExecuteBuffer();    // 执行缓冲区命令
+
+    }
+    /// <summary>
+    /// 执行缓冲区命令：使用和清除缓冲区通常是配套使用的
+    /// </summary>
+    void ExecuteBuffer()
+    {
+        context.ExecuteCommandBuffer(buffer);
+        buffer.Clear();
+    }
+    static ShaderTagId unlitShaderTagId = new ShaderTagId("SRPDefaultUnlit");
+
 
     /// <summary>
-    /// 绘制SRP不支持的着色器类型
+    /// 绘制可见物
     /// </summary>
-    partial void DrawUnsupportedShaders()
+    void DrawVisibleGeometry()
     {
-        // 不支持的ShaderTag类型我们使用错误材质专用Shader来渲染（粉色颜色）
-        if (errorMaterial == null)
+        // 设置绘制顺序和指定渲染相机
+        var sortingSettings = new SortingSettings(camera)
         {
-            errorMaterial = new Material(Shader.Find("Hidden/InternalErrorShader"));
-        }
-
-
-        // 数组第一个元素用来构造DrawingSettings对象的时候设置
-        var drawingSettings = new DrawingSettings(legacyShaderTagIds[0], new SortingSettings(camera))
-        {
-            overrideMaterial = errorMaterial
+            criteria = SortingCriteria.CommonOpaque
         };
-        for(int i = 1; i <legacyShaderTagIds.Length; i++)
-        {
-            // 遍历数组逐个设置着色器的PassName，从i=1开始
-            drawingSettings.SetShaderPassName(i, legacyShaderTagIds[i]);
-        }
-        // 使用默认设置即可，反正画出来的都是不支持的
-        var filteringSettings = FilteringSettings.defaultValue;
-        // 绘制不支持的shadertag类型的物体
+        // 设置绘制的shader Pass 和排序模式
+        var drawingSettings = new DrawingSettings(unlitShaderTagId, sortingSettings);
+        // 设置哪些类型的渲染队列可以被绘制
+        var filteringSettings = new FilteringSettings(RenderQueueRange.all);
+
+        // 1.绘制不透明物体
+        context.DrawRenderers(cullingResults, ref drawingSettings, ref filteringSettings);
+
+        // 2.绘制天空盒
+        context.DrawSkybox(camera);
+
+        sortingSettings.criteria = SortingCriteria.CommonTransparent;
+        drawingSettings.sortingSettings = sortingSettings;
+        // 只绘制RenderQueue为Transparent的物体
+        filteringSettings.renderQueueRange = RenderQueueRange.transparent;
+        // 3.绘制透明物体
         context.DrawRenderers(cullingResults, ref drawingSettings, ref filteringSettings);
     }
-#endif
+    /// <summary>
+    /// 提交缓冲区渲染命令
+    /// </summary>
+    void Submit()
+    {
+        buffer.EndSample(SampleName);
+        ExecuteBuffer();
+        context.Submit();       // 提交缓冲区渲染命令才进行这一帧的渲染
+    }
+    // 缓冲区，用来绘制场景的其它几何图像
+    const string bufferName = "My Render Camera";
+    CommandBuffer buffer = new CommandBuffer
+    {
+        name = bufferName
+    };
+
+    // 存储剔除后的结果数据
+    CullingResults cullingResults;
+    /// <summary>
+    /// 剔除
+    /// </summary>
+    /// <returns></returns>
+    bool Cull()
+    {
+        ScriptableCullingParameters p;
+        // 获取相机的剔除参数
+        if (camera.TryGetCullingParameters(out p))
+        {
+            // 剔除
+            cullingResults = context.Cull(ref p);
+            return true;
+        }
+        return false;
+    }
+
+
+    
 }
