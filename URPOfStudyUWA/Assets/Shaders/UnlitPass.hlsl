51dde65a69c41b5f3607993f5ae9b034a85f304e
liujianjie <liujianjie@galasports.com>
2024-02-23 13:11:36 +0800

    2.DrawCall 材质添加对纹理的支持

--- URPOfStudyUWA/Assets/Shaders/UnlitPass.hlsl
+++ URPOfStudyUWA/Assets/Shaders/UnlitPass.hlsl
@@ -3,6 +3,20 @@
 
 #include "ShaderLibrary/Common.hlsl"
 
+// 所有材质的属性我们需要在常量缓冲区里定义
+//CBUFFER_START(UnityPerMaterial)
+//    float4 _BaseColor;
+//CBUFFER_END
+
+// 纹理采样器
+TEXTURE2D(_BaseMap);            // 定义一张纹理
+SAMPLER(sampler_BaseMap);       // 为上一个定义的纹理提供采样器
+
+UNITY_INSTANCING_BUFFER_START(UnityPerMaterial)
+UNITY_DEFINE_INSTANCED_PROP(float4, _BaseMap_ST) // 提供纹理的缩放和平移
+UNITY_DEFINE_INSTANCED_PROP(float4, _BaseColor)
+UNITY_INSTANCING_BUFFER_END(UnityPerMaterial)
+
 // 用作顶点函数的输入参数
 struct Attributes
 {
@@ -27,31 +41,18 @@ Varyings UnlitPassVertex(Attributes input)
     float3 positionWS = TransformObjectToWorld(input.positionOS);
     output.positionCS = TransformWorldToHClip(positionWS);
     // 计算缩放和片以后的UV坐标
-    //float4 baseST = UNITY_ACCESS_INSTANCED_PROP(UnityPerMaterial, _BaseMap_ST);
-    //output.baseUV = input.baseUV * baseST.xy + baseST.zw;
+    float4 baseST = UNITY_ACCESS_INSTANCED_PROP(UnityPerMaterial, _BaseMap_ST);
+    output.baseUV = input.baseUV * baseST.xy + baseST.zw;
     return output;
 }
-// 所有材质的属性我们需要在常量缓冲区里定义
-//CBUFFER_START(UnityPerMaterial)
-//    float4 _BaseColor;
-//CBUFFER_END
-
-// 纹理采样器
-TEXTURE2D(_BaseMap);            // 定义一张纹理
-SAMPLER(sampler_BaseMap);       // 为上一个定义的纹理提供采样器
-
-UNITY_INSTANCING_BUFFER_START(UnityPerMaterial)
-UNITY_DEFINE_INSTANCED_PROP(float4, _BaseMap_ST) // 提供纹理的缩放和平移
-UNITY_DEFINE_INSTANCED_PROP(float4, _BaseColor)
-UNITY_INSTANCING_BUFFER_END(UnityPerMaterial)
 
 // 片元函数
 float4 UnlitPassFragment(Varyings input) : SV_TARGET
 {
     UNITY_SETUP_INSTANCE_ID(input);
-    //float4 baseMap = SAMPLE_TEXTURE2D(_BaseMap, sampler_BaseMap, input.baseUV); // 采样纹理
+    float4 baseMap = SAMPLE_TEXTURE2D(_BaseMap, sampler_BaseMap, input.baseUV); // 采样纹理
     float4 baseColor = UNITY_ACCESS_INSTANCED_PROP(UnityPerMaterial, _BaseColor);
-    return baseColor;
+    return baseColor * baseMap;
 
 }
 
