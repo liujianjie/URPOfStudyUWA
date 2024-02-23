a66eeab873d4f05e38b98cdd7c787061843e8262
liujianjie <liujianjie@galasports.com>
2024-02-23 13:09:25 +0800

    2.DrawCall 混合

--- URPOfStudyUWA/Assets/Shaders/Unlit.shader
+++ URPOfStudyUWA/Assets/Shaders/Unlit.shader
@@ -1,13 +1,22 @@
 Shader "CustomeRP/Unlit"
 {
     Properties{
-
+        _BaseMap("Texture", 2D) = "white" {}
         _BaseColor("Color", Color) = (1.0, 1.0, 1.0, 1.0)
+        // 设置混合模式
+        [Enum(UnityEngine.Rendering.BlendMode)] _SrcBlend("Src Blend", Float) = 1
+        [Enum(UnityEngine.Rendering.BlendMode)] _DstBlend("Dst Blend", Float) = 0
+        // 默认写入深度缓冲区
+        [Enum(Off, 0, On, 1)] _ZWrite("Z Write", Float) = 1
     }
 
     SubShader
     {
         Pass{
+            // 定义混合而陌生
+            Blend[_SrcBlend][_DstBlend]
+            // 是否写入深度
+            ZWrite[_ZWrite]
 	        HLSLPROGRAM
             #pragma multi_compile_instancing
 	        #pragma vertex UnlitPassVertex
--- URPOfStudyUWA/Assets/Shaders/UnlitPass.hlsl
+++ URPOfStudyUWA/Assets/Shaders/UnlitPass.hlsl
@@ -7,12 +7,13 @@
 struct Attributes
 {
     float3 positionOS : POSITION;
+    float2 baseUV : TEXCOORD;
     UNITY_VERTEX_INPUT_INSTANCE_ID
 };
 // 用作片元函数的输入参数
 struct Varyings
 {
-    float4 positionCS : SV_Position;
+    float4 positionCS : SV_POSITION;
     float2 baseUV : VAR_BASE_UV;
     UNITY_VERTEX_INPUT_INSTANCE_ID
 };
@@ -25,6 +26,9 @@ Varyings UnlitPassVertex(Attributes input)
     UNITY_TRANSFER_INSTANCE_ID(input, output);
     float3 positionWS = TransformObjectToWorld(input.positionOS);
     output.positionCS = TransformWorldToHClip(positionWS);
+    // 计算缩放和片以后的UV坐标
+    //float4 baseST = UNITY_ACCESS_INSTANCED_PROP(UnityPerMaterial, _BaseMap_ST);
+    //output.baseUV = input.baseUV * baseST.xy + baseST.zw;
     return output;
 }
 // 所有材质的属性我们需要在常量缓冲区里定义
@@ -32,7 +36,12 @@ Varyings UnlitPassVertex(Attributes input)
 //    float4 _BaseColor;
 //CBUFFER_END
 
+// 纹理采样器
+TEXTURE2D(_BaseMap);            // 定义一张纹理
+SAMPLER(sampler_BaseMap);       // 为上一个定义的纹理提供采样器
+
 UNITY_INSTANCING_BUFFER_START(UnityPerMaterial)
+UNITY_DEFINE_INSTANCED_PROP(float4, _BaseMap_ST) // 提供纹理的缩放和平移
 UNITY_DEFINE_INSTANCED_PROP(float4, _BaseColor)
 UNITY_INSTANCING_BUFFER_END(UnityPerMaterial)
 
@@ -40,7 +49,10 @@ UNITY_INSTANCING_BUFFER_END(UnityPerMaterial)
 float4 UnlitPassFragment(Varyings input) : SV_TARGET
 {
     UNITY_SETUP_INSTANCE_ID(input);
-    return UNITY_ACCESS_INSTANCED_PROP(UnityPerMaterial, _BaseColor);
+    //float4 baseMap = SAMPLE_TEXTURE2D(_BaseMap, sampler_BaseMap, input.baseUV); // 采样纹理
+    float4 baseColor = UNITY_ACCESS_INSTANCED_PROP(UnityPerMaterial, _BaseColor);
+    return baseColor;
+
 }
 
 
