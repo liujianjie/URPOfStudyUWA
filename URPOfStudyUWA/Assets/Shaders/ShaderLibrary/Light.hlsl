3f4bfdec3560a41cfe7cde15b69c6a9d6c9a7a86
liujianjie <liujianjie@galasports.com>
2024-03-07 10:01:59 +0800

    4.2.4灯光的阴影衰减

--- URPOfStudyUWA/Assets/Shaders/ShaderLibrary/Light.hlsl
+++ URPOfStudyUWA/Assets/Shaders/ShaderLibrary/Light.hlsl
@@ -20,29 +20,34 @@ struct Light
 {
     float3 color;
     float3 direction;
+    float attenuation;  // 光源的阴影衰减属性
 };
+// 获取方向光的阴影数据
+DirectionalShadowData GetDirectinalShadowData(int lightIndex)
+{
+    DirectionalShadowData data;
+    data.strength = _DirectionalLightShadowData[lightIndex].x;
+    data.tileIndex = _DirectionalLightShadowData[lightIndex].y;
+    return data;
+}
+
 // 获取方向光的数量
 int GetDirectionalLightCount()
 {
     return _DirectionalLightCount;
 }
 // 获取指定索引的方向光的数据
-Light GetDirectionalLight(int index)
+Light GetDirectionalLight(int index, Surface surfaceWS)
 {
     Light light;
     light.color = _DirectionalLightColors[index].rgb;
     // light.color = float3(0.8, 0.5, 1);
     light.direction = _DirectionalLightDirections[index].xyz;
+    // 得到阴影数据
+    DirectionalShadowData shadowData = GetDirectinalShadowData(index);
+    // 得到阴影衰减
+    light.attenuation = GetDirectionalShadowAttenuation(shadowData, surfaceWS);
     return light;
 }
 
-// 获取方向光的数据
-//Light GetDirectionalLight()
-//{
-//    Light light;
-    //light.color = float3(0.8, 1, 1);
-//    light.color = _DirectionalLightColor;
-//    light.direction =_DirectionalLightDirection;
-//    return light;
-//}
 #endif
--- URPOfStudyUWA/Assets/Shaders/ShaderLibrary/Lighting.hlsl
+++ URPOfStudyUWA/Assets/Shaders/ShaderLibrary/Lighting.hlsl
@@ -17,26 +17,23 @@ float3 DirectBRDF(Surface surface, BRDF brdf, Light light)
 // 计算入射光照
 float3 IncomingLight(Surface surface, Light light)
 {
-    return saturate(dot(surface.normal, light.direction)) * light.color;
+    return saturate(dot(surface.normal, light.direction) * light.attenuation) * light.color;
 }
 // 入射光照乘以表面颜色，得到最终的照明颜色
 float3 GetLighting(Surface surface, BRDF brdf, Light light)
 {
     return IncomingLight(surface, light) * DirectBRDF(surface, brdf, light);    // 入射光*表面颜色
 }
-// 获取最终照明结果
-float3 GetLighting(Surface surface, BRDF brdf)
+// 根据物体的表面信息和灯光属性获取最终光照结果
+float3 GetLighting(Surface surfaceWS, BRDF brdf)
 {
     // 可见方向光的照明结果进行累加得到最终照明结果
     float3 color = 0.0;
     for (int i = 0; i < GetDirectionalLightCount(); i++)
     {
-        color += GetLighting(surface, brdf, GetDirectionalLight(i));
+        color += GetLighting(surfaceWS, brdf, GetDirectionalLight(i, surfaceWS));
     }
     return color;
 }
-
-
-
 
 #endif
