1b2175abf758ddd6580ed3022e190a1077e2fc3c
liujianjie <liujianjie@galasports.com>
2024-05-24 13:08:52 +0800

    修复第4章：阴影的bug

--- URPOfStudyUWA/Assets/CustomRP/Runtime/Shadows.cs
+++ URPOfStudyUWA/Assets/CustomRP/Runtime/Shadows.cs
@@ -117,7 +117,7 @@ public class Shadows
     /// </summary>
     /// <param name="light"></param>
     /// <param name="visibleLightIndex"></param>
-    public Vector2 ReserveDirectionalShadows(Light light, int visibleLightIndex)
+    public Vector3 ReserveDirectionalShadows(Light light, int visibleLightIndex)
     {
         // 存储可见光源的索引，前提是光源开启了阴影投射并且阴影强度不能为0
         // 是否在阴影最大投射距离内，有被该光源影响并且需要投射的物体存在，如果没有就不需要渲染该光源的阴影贴图了
@@ -129,7 +129,7 @@ public class Shadows
             // 返回阴影强度和阴影图块索引
             return new Vector3(light.shadowStrength, settings.directional.cascadeCount * ShadowedDirectionalLightCount++, light.shadowNormalBias);
         }
-        return Vector2.zero;
+        return Vector3.zero;
     }
 
     // 阴影渲染
