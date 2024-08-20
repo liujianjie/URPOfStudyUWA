d22d1d07aa102ac9bf89eefdd6a9fec5d665f150
liujianjie <liujianjie@galasports.com>
2024-08-20 12:58:02 +0800

    10.1.2堆栈对象

---
+++ URPOfStudyUWA/Assets/CustomRP/Runtime/PostFXStack.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Rendering;
+
+public class PostFXStack 
+{
+    const string bufferName = "Post FX";
+    
+    CommandBuffer buffer = new CommandBuffer
+    {
+        name = bufferName
+    };
+
+    ScriptableRenderContext context;
+    Camera camera;
+    PostFXSettings settings;
+
+    // 判断是否需要应用后处理效果
+     public bool IsActive => settings != null;
+
+    public void Setup(ScriptableRenderContext context, Camera camera, PostFXSettings settings)
+    {
+        this.context = context;
+        this.camera = camera;
+        this.settings = settings;
+
+    }
+    /// <summary>
+    /// 渲染后处理特效
+    /// </summary>
+    /// <param name="sourceId"></param>
+    public void Render(int sourceId)
+    {
+        buffer.Blit(sourceId, BuiltinRenderTextureType.CameraTarget);
+        context.ExecuteCommandBuffer(buffer);
+        buffer.Clear();
+    }
+}
